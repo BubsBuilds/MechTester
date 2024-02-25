f73d56f38c54e3333b13a176f1a358c8f372c0c3
bubsbuilds <bubsbuilds@jfs-agri.com>
2024-02-24 19:02:12 -0700

    Current status

--- main.py
+++ main.py
@@ -1,111 +1,119 @@
 #!/usr/bin/env python3
 from datetime import datetime
 
-from nicegui import run, ui
+from nicegui import app, run, ui
 import csv
 import gpiozero
 import pymongo
 import time
 
 class dbComm:
-  def __init__(self, host="JFS-MAIN", port=27017, db_name='mt'):
-    self.client = pymongo.MongoClient(host, port)
-    self.db = self.client[db_name]
+    def __init__(self, host="JFS-MAIN", port=27017, db_name='mt'):
+        self.client = pymongo.MongoClient(host, port)
+        self.db = self.client[db_name]
+
 
 class lcComm:
-  def __init__(self, dbComm):
-    self.lc_const = 2940 / 4194304
-    self.lc_offset = 0
-    self.DT_PIN = 10
-    self.SCK_PIN = 22
-    self.sck = gpiozero.OutputDevice(self.SCK_PIN)
-    self.dt = gpiozero.InputDevice(self.DT_PIN)
-    self.vals = []
-    self.times = []
-    self.dbComm = dbComm
-    # Create document for session data
-    rec = {
-      'time': time.time(),
-      'lcDat': [],
-    }
-    self.coll = self.dbComm.db['test_data']
-    self.datRec = self.coll.insert_one(rec).inserted_id
-  def getLC(self):
-    try:
-      while getLCstat == 'ON':
-        Count = 0
-        self.sck.off()
-        while self.dt.value == 1:
-          time.sleep(0.005)
-        for i in range(24):
-          start_counter = time.perf_counter()
-          self.sck.on()
-          self.sck.off()
-          end_counter = time.perf_counter()
-          time_elapsed = float(end_counter - start_counter)
-          Count = (Count << 1) | self.dt.value
 
-        # calculate int from 2's complement
-        signed_data = 0
-        if (Count & 0x800000):  # 0b1000 0000 0000 0000 0000 0000 check if the sign bit is 1. Negative number.
-          signed_data = -((Count ^ 0xffffff) + 1)  # convert from 2's complement to int
-        else:  # else do not do anything the value is positive number
-          signed_data = Count
-        self.curLCval = round(float(signed_data * self.lc_const), 3)  - self.lc_offset
-        self.curTime = round(time.time(), 3)
-        self.coll.update_one({'_id': self.datRec}, {'$push': [self.curTime, self.curLCval]})
-        time.sleep(0.2)
-        #print(load)
-    except:
-      print('Get LC Fail')
+    def __init__(self):
+        self.active = True
+        self.lc_const = 2940 / 4194304
+        self.lc_offset = 0
+        self.DT_PIN = 10
+        self.SCK_PIN = 22
+        self.sck = gpiozero.OutputDevice(self.SCK_PIN)
+        self.dt = gpiozero.InputDevice(self.DT_PIN)
+        self.vals = []
+        self.times = []
+        self.curLCval = 0
+        self.curTime = time.time()
 
-  def tare(self):
-    self.lc_offset = self.curLCval
+    def initdb(self, db):
+        self.dbComm = db
+        # Create document for session data
+        rec = {
+            'time': time.time(),
+            'lcDat': [],
+        }
+        self.coll = self.dbComm.db['test_data']
+        self.datRec = self.coll.insert_one(rec).inserted_id
 
-    outfilepath = f'./TestResult_{round(time.time())}.csv'
-    with open(outfilepath, 'w', newline='') as file:
-      writer = csv.writer(file)
-      for i, load in enumerate(vals):
-        writer.writerow([times[i], load])
+    def getLC(self):
+        # try:
+          while self.active:
+              Count = 0
+              self.sck.off()
+              while self.dt.value == 1:
+                  time.sleep(0.005)
+              for i in range(24):
+                  start_counter = time.perf_counter()
+                  self.sck.on()
+                  self.sck.off()
+                  end_counter = time.perf_counter()
+                  time_elapsed = float(end_counter - start_counter)
+                  Count = (Count << 1) | self.dt.value
+
+              # calculate int from 2's complement
+              signed_data = 0
+              if (Count & 0x800000):  # 0b1000 0000 0000 0000 0000 0000 check if the sign bit is 1. Negative number.
+                  signed_data = -((Count ^ 0xffffff) + 1)  # convert from 2's complement to int
+              else:  # else do not do anything the value is positive number
+                  signed_data = Count
+              self.curLCval = round(float(signed_data * self.lc_const), 3) - self.lc_offset
+              self.curTime = round(time.time(), 3)
+              self.coll.update_one({'_id': self.datRec}, {'$push': {'lcDat': [self.curTime, self.curLCval]}})
+              #lcPlot.push([self.curTime], [[self.curLCval]])
+              #lcVal.set_text(str(self.curLCval))
+              time.sleep(0.2)
+              # print(load)
+        # except:
+        #     print('Get LC Fail')
+
+    def tare(self):
+        self.lc_offset = self.curLCval
+
 def setMotors(dir, val):
-  pass
-#
-# async def LoadCellDatLoop():
-#   pass
+    pass
 
-async def lcGetter():
-  result = await run.cpu_bound(lcComm.getLC())
+def updateLinePlot():
+    lcPlot.push([lcSess.curTime], [[lcSess.curLCval]])
+    lcVal.set_text(str(lcSess.curLCval))
 
-def updateLinePlot() -> None:
-  lcPlot.push([lcComm.curTime], [[lcComm.curLCval]])
-  lcVal.set_text(str(lcComm.curLCval))
+def callGetLC():
+    lcSess.getLC()
 
-dbComm = dbComm()
-lcComm = lcComm(dbComm)
-getLCstat = ui.label('ON')
-curLCval = 0
-ui.dark_mode().enable()
+
+async def startup(db, lc):
+    global dbSess
+    global lcSess
+    dbSess = db
+    lcSess = lc
+    lcComm.initdb(dbSess)
+    ui.dark_mode().enable()
+    result = await run.cpu_bound(callGetLC)
+
+
+app.on_startup(startup(dbComm(), lcComm()))
+
 ui.label('Mech Tester').classes('text-h1')
 ui.separator()
 with ui.column():
-  with ui.card():
-    ui.label('Manual Actuator Controls')
-    ui.separator()
-    motorSpeed = ui.slider(min=0, max=100, value=80)
-    ui.label().bind_text_from(motorSpeed, 'value')
-    ui.button('UP', on_click=lambda: setMotors(1, motorSpeed.value))
-    ui.button('DOWN', on_click=lambda: setMotors(-1, motorSpeed.value))
+    with ui.card():
+        ui.label('Manual Actuator Controls')
+        ui.separator()
+        motorSpeed = ui.slider(min=0, max=100, value=80)
+        ui.label().bind_text_from(motorSpeed, 'value')
+        ui.button('UP', on_click=lambda: setMotors(1, motorSpeed.value))
+        ui.button('DOWN', on_click=lambda: setMotors(-1, motorSpeed.value))
 
-  with ui.card():
-    ui.label('Load Cell')
-    ui.separator()
-    lcVal = ui.label('0')
-    #lcVal = ui.label().classes('text-h3').bind_text(target_object= getLC, target_name='curLCval')
-    lcPlot = ui.line_plot(n=1, limit=100, figsize=(15, 10), update_every=1)
-    ui.button('TARE', on_click=lambda: lcComm.tare)
-
-ui.button('Stop LC', on_click=lambda getLCstat: False )
-
-
+    with ui.card():
+        ui.label('Load Cell')
+        ui.separator()
+        lcVal = ui.label('0')
+        # lcVal = ui.label().classes('text-h3').bind_text(target_object= getLC, target_name='curLCval')
+        lcPlot = ui.line_plot(n=1, limit=100, figsize=(10, 5), update_every=5)
+        ui.button('TARE', on_click=lambda: lcComm.tare)
+line_updates = ui.timer(0.5, updateLinePlot, active=True)
+# ui.button('Stop LC', on_click=lambda getLCstat: False)
 
 ui.run()
