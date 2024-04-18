332c80d69ea6650557d942f78fc3883321c5b6b5
bubs <bubsbuilds@jfs-agri.com>
2024-04-18 00:15:39 -0600

    Updated both main2.py and arduino firmware for closed loop functionality. Still need to complete db integration.

--- main2.py
+++ main2.py
@@ -10,14 +10,23 @@ import serial
 import time
 
 class dbComm:
-    def __init__(self, host="JFS-MAIN", port=27017, db_name='mt'):
+    def __init__(self, host="192.168.50.41", port=27017, db_name='mt'):
         self.client = pymongo.MongoClient(host, port)
         self.db = self.client[db_name]
+        self.dbSessColl = self.db['mt_sess']
+        self.datColl = self.db['mt_dat']
+        # Create document for session data
+        self.sessHead = {
+            'ts': time.time(),
+            'data_coll': 'mt_dat',
+        }
+        self.sessRecID = self.dbSessColl.insert_one(self.sessHead).inserted_id
 
 class mechController:
     def __init__(self, serial_port='/dev/ttyACM0', baud_rate=9600):
         self.ser = serial.Serial(serial_port, baud_rate)
         self.log_file = "command_log.json"
+        self.dbSess = dbComm()
         self.commands_log = []
         self.lcCal = 2940 / 4194304
         self.lcVals = []
@@ -30,8 +39,19 @@ class mechController:
         self.ser.write(command.encode())
         time.sleep(0.1)
         confirmation = self.ser.readline().decode().strip()
+        retList = self.parse_command(confirmation)
         self.log_command(command, confirmation)
-        return confirmation
+        return retList
+
+    def parse_command(self, message):
+        # Remove the leading and trailing characters ('<<' and '>>')
+        cleaned_string = message.strip("<<>>")
+
+        # Split the cleaned string by '><' to separate id and value
+        id_value_list = cleaned_string.split("><")
+
+        # Return the list in the desired format
+        return id_value_list
 
     def log_command(self, command, confirmation):
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -46,7 +66,8 @@ class mechController:
         self.send_command(command)
 
     def read_scale(self):
-        newVal = float(self.send_command("HXREAD\n").split(" ")[2])
+        ret = self.send_command("HXREAD\n")
+        newVal = float(self.send_command("HXREAD\n")[1])
         self.lcVal = newVal * self.lcCal
         self.lcTime = time.time()
         self.lcVals.append(self.lcVal)
@@ -55,12 +76,30 @@ class mechController:
     def tare_scale(self):
         self.send_command("HXTARE\n")
 
+    def load_test(self, test_type, limit):
+        runL = True
+        if test_type == 1:
+            test_type = "C"
+        elif test_type == 2:
+            test_type = "T"
+        self.send_command(f"CL{test_type}{round(limit * self.lcCal)}\n")
+        while runL:
+            if self.ser.in_waiting:
+                response = self.ser.readline()
+                parsed = self.parse_command(response)
+                print(parsed)
+                rec = {
+                    parsed[0]: parsed[1],
+                }
+                self.dbSess.datColl.insert_one(rec)
+                runL = False
+
     def close(self):
         self.ser.close()
 
 def updateLinePlot(mcSess):
     mcSess.read_scale()
-    time.sleep(0.2)
+    #time.sleep(0.2)
     lcPlot.push(mcSess.lcTimes, [mcSess.lcVals])
     lcVal.set_text(str(mcSess.lcVal))
 
@@ -68,7 +107,7 @@ async def startup():
 
     ui.dark_mode().enable()
 
-dbSess = dbComm()
+#dbSess = dbComm()
 mcSess = mechController()
 app.on_startup(startup())
 
@@ -80,8 +119,13 @@ with ui.column():
         ui.separator()
         motorSpeed = ui.slider(min=0, max=100, value=80)
         ui.label().bind_text_from(motorSpeed, 'value')
-        ui.button('UP', on_click=lambda: mcSess.set_motor('A', 'F', int(motorSpeed.value)))
-        ui.button('DOWN', on_click=lambda: mcSess.set_motor('A', 'R', int(motorSpeed.value)))
+        ui.button('UP', on_click=lambda: mcSess.set_motor('A', 'R', int(motorSpeed.value)))
+        ui.button('DOWN', on_click=lambda: mcSess.set_motor('A', 'F', int(motorSpeed.value)))
+        ui.button('STOP', on_click=lambda: mcSess.set_motor('A','F', int(0)))
+        lt_type = ui.radio({1: 'Compression', 2: 'Tension'}, value=1).props('inline')
+        lt_limit = ui.slider(min=-500, max=500, value=0)
+        ui.label().bind_text_from(lt_limit, 'value')
+        ui.button('RUN', on_click=lambda: mcSess.load_test(lt_type.value, lt_limit.value))
 
     with ui.card():
         ui.label('Load Cell')
--- mcu/sketch_mar23a/sketch_mar23a.ino
+++ mcu/sketch_mar23a/sketch_mar23a.ino
@@ -2,19 +2,23 @@
 #include <HX711.h>
 
 // Motor A
-const int motorA1 = 2;
-const int motorA2 = 3;
-const int motorAPWM = 5;
+const int motorA1 = 7;
+const int motorA2 = 8;
+const int motorAPWM = 11;
 
-// Motor B
-const int motorB1 = 4;
-const int motorB2 = 7;
-const int motorBPWM = 6;
+// // Motor B
+// const int motorB1 = 4;
+// const int motorB2 = 7;
+// const int motorBPWM = 6;
 
 // HX711 pins
-const int LOADCELL_DOUT_PIN = 9;
-const int LOADCELL_SCK_PIN = 8;
+const int LOADCELL_DOUT_PIN = 4;
+const int LOADCELL_SCK_PIN = 5;
+long lcVal;
 HX711 scale;
+
+int dispSenseVal;
+int dispSensePin = A0;
 
 void setup() {
   Serial.begin(9600);
@@ -22,9 +26,9 @@ void setup() {
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorAPWM, OUTPUT);
-  pinMode(motorB1, OUTPUT);
-  pinMode(motorB2, OUTPUT);
-  pinMode(motorBPWM, OUTPUT);
+  // pinMode(motorB1, OUTPUT);
+  // pinMode(motorB2, OUTPUT);
+  // pinMode(motorBPWM, OUTPUT);
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 }
@@ -37,6 +41,8 @@ void loop() {
       handleMotorCommand(command);
     } else if (command.startsWith("HX")) {
       handleScaleCommand(command);
+    } else if (command.startsWith("CL")){
+      handleLCCommand(command);
     }
   }
   //long reading = scale.read();
@@ -49,7 +55,7 @@ void handleMotorCommand(String command) {
   if (command.startsWith("MA")) {
     pin1 = motorA1; pin2 = motorA2; pwmPin = motorAPWM;
   } else {
-    pin1 = motorB1; pin2 = motorB2; pwmPin = motorBPWM;
+    // pin1 = motorB1; pin2 = motorB2; pwmPin = motorBPWM;
   }
 
   char dir = command.charAt(2);
@@ -58,28 +64,70 @@ void handleMotorCommand(String command) {
   if (dir == 'R') {
     digitalWrite(motorA1, HIGH);
     digitalWrite(motorA2, LOW);
-    digitalWrite(motorB1, HIGH);
-    digitalWrite(motorB2, LOW);
+    // digitalWrite(motorB1, HIGH);
+    // digitalWrite(motorB2, LOW);
 
-  } else {
+  } else if (dir == 'F') {
     digitalWrite(motorA1, LOW);
     digitalWrite(motorA2, HIGH);
-    digitalWrite(motorB1, LOW);
-    digitalWrite(motorB2, HIGH);
+    // digitalWrite(motorB1, LOW);
+    // digitalWrite(motorB2, HIGH);
+  } else {
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, LOW);
+    speed = 0;
   }
   analogWrite(motorAPWM, speed);
-  analogWrite(motorBPWM, speed);
+  // analogWrite(motorBPWM, speed);
   
-  Serial.println("Command executed: " + command);
+  //Serial.println("Command executed: " + command);
+  Serial.println("<<ma><complete>>");
 }
 
 void handleScaleCommand(String command) {
   if (command.substring(2) == "READ") {
-    long reading = scale.read();
+    lcVal = scale.read();
     delay(200);
-    Serial.println("HX711 Reading: " + String(reading));
+    //Serial.println("HX711 Reading: " + String(lcVal));
+    Serial.println("<<lc><"+String(lcVal)+">>");
   } else if (command.substring(2) == "TARE") {
     scale.tare();
     Serial.println("HX711 Tare done.");
+  } else if (command.substring(2) == "ALL") {
+    lcVal = scale.read();
+    delay(200);
+    //Serial.println("HX711 Reading: " + String(lcVal));
+    Serial.println("<<lc><"+String(lcVal)+">>");
+    dispSenseVal = analogRead(dispSensePin);
+    Serial.println("<<ds><"+String(dispSenseVal)+">>");
+  }
+}
+
+void handleLCCommand(String command){
+  char dir = command.charAt(2);
+  long target = command.substring(3).toInt();
+  bool cont = true;
+  while (cont == true) {
+    handleScaleCommand("HXREAD");
+    
+    if (dir == 'T'){
+      handleMotorCommand("MAR175");
+      delay(25);
+      handleMotorCommand("MAS0");
+      if (lcVal >= target){
+        cont = false;
+        handleMotorCommand("MAS0");
+        Serial.println("<<cl><end>>");
+      }
+    }else if (dir == 'C'){
+      handleMotorCommand("MAF175");
+      delay(25);
+      handleMotorCommand("MAS0");
+      if (lcVal <= target){
+        cont = false;
+        handleMotorCommand("MAS0");
+        Serial.println("<<cl><end>>");
+      }
+    }
   }
 }
